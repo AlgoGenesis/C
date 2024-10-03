d45976fb4f3b3de34bd2b302f57c06cadf4b7e45
icemberg <135527820+icemberg@users.noreply.github.com>
2024-10-03 20:38:53 +0530

    Update program.c

--- Mathematical Algorithms/Euclidean Algorithm/program.c
+++ Mathematical Algorithms/Euclidean Algorithm/program.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int Euclid(int m, int n) {
+    // Computes gcd(m, n) by Euclid's algorithm
+    while (n != 0) {
+        int r = m % n;
+        m = n;
+        n = r;
+    }
+    return m;
+}
+
+int main() {
+    int m, n;
+    printf("Enter two nonnegative integers: ");
+    scanf("%d %d", &m, &n);
+    int gcd = Euclid(m, n);
+    printf("Greatest common divisor of %d and %d is: %d\n", m, n, gcd);
+    return 0;
+}
