4a1f12eb023274b3ae28c47ffcf9928276a2baca
Pooja N R <157814064+poojapoojanr@users.noreply.github.com>
2024-10-03 09:22:56 +0530

    Update Program.c

--- Dynamic Programming/MF-Knapsack/Program.c
+++ Dynamic Programming/MF-Knapsack/Program.c
@@ -1,1 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int max(int a, int b){
+ return (a>b) ? a : b;
+}
+
+int t[100][100], v[100], w[100], n, m, i, j;
+
+int knap(int i, int j){
+ if (t[i][j]==-1){
+  if (j<w[i])
+   t[i][j] = knap(i-1,j);
+ else
+   t[i][j] = max(knap(i-1,j),v[i]+knap(i-1,j-w[i]));
+}
+   return t[i][j];
+}
+
+
+void main(){
+printf("No. of Items>> ");
+scanf("%d",&n);
+printf("Sack Capacity>> ");
+scanf("%d",&m);
+printf("Weight\tValue\n");
+for(i=1;i<n+1;i++){scanf("%d\t%d",&w[i],&v[i]);
+}
+
+
+for(i=0;i<n+1;i++){
+  for(j=0;j<m+1;j++){
+    if (i==0||j==0)
+      t[i][j]=0;
+    else
+      t[i][j]=-1;
+  }
+}
+
+printf("Maximum Value: %d\n",knap(n,m));
+for(int i=0;i<n+1;i++){
+  for(int j=0;j<m+1;j++)
+    printf("%d ",t[i][j]);
+    printf("\n");
+}
+//  to print which are the sacks added 
+printf("Composition:\n");
+for(i=n;i>0;i--){
+ if (t[i][m] != t[i-1][m]){
+  printf("%d ",i);
+   m = m-w[i];
+  }
+}
+ printf("\n");
+}
