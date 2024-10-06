2daf0121627038888236944fbfc5f35370ebda11
Utsav Kashyap <126500753+UtsavKashyap1710@users.noreply.github.com>
2024-10-06 22:06:43 +0530

    Update Program.c

--- Searching Algorithms/Binary Search/Program.c
+++ Searching Algorithms/Binary Search/Program.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+// use this more optimize fxn
+int find_position(const vector<int>& a, int x) {
+    auto f = [&](int i) {
+        return a[i] < x;
+    };
+    
+    int n = (int)a.size();
+    int pos = find_first_false(0, n - 1, f);
+    
+    if (pos == n || a[pos] != x) return n;  // If x not found, return n
+    return pos;  // Return position of x
+}
 // Function to perform binary search
 int binarySearch(int arr[], int n, int x) {
     int left = 0;
@@ -33,6 +45,13 @@ int main() {
 
     printf("Enter a number to search: ");
     scanf("%d", &x);
+    int pos = find_position(a, x);
+    
+    if (pos != (int)a.size()) {
+        cout << "Element " << x << " found at index: " << pos << endl;
+    } else {
+        cout << "Element " << x << " not found" << endl;
+    }
 
     int result = binarySearch(arr, n, x);
     if (result != -1) {
