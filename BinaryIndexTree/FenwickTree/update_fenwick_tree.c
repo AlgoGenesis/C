c55e601522f9114a5ad8d53ccf586d887b52c45e
Mahraib Fatima <122329300+MahraibFatima@users.noreply.github.com>
2024-10-10 18:56:22 +0000

    update_fenwick_tree deleted

--- BinaryIndexTree/FenwickTree/update_fenwick_tree.c
+++
@@ -1,8 +0,0 @@
-void updateFenwickTree(int fenwick[], int n, int index, int value) {
-    index += 1;  // Fenwick Tree is 1-indexed
-
-    while (index <= n) {
-        fenwick[index] += value;
-        index += index & -index;
-    }
-}
