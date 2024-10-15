431e494a0c656c5d399915deb7e240dcfab6b8e3
K N Meghana <meghanakn473@gmail.com>
2024-10-15 20:50:27 +0530

    Added sjf cpu scheduling

---
+++ SJF-CPU-Scheduling/Program.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+void findWaitingTime(int processes[], int n, int bt[], int wt[]) {
+    wt[0] = 0; // Waiting time for the first process is 0
+
+    // Calculate waiting time for each process
+    for (int i = 1; i < n; i++) {
+        wt[i] = bt[i - 1] + wt[i - 1];
+    }
+}
+
+void findTurnAroundTime(int processes[], int n, int bt[], int wt[], int tat[]) {
+    // Calculate turnaround time for each process
+    for (int i = 0; i < n; i++) {
+        tat[i] = bt[i] + wt[i];
+    }
+}
+
+void findavgTime(int processes[], int n, int bt[]) {
+    int wt[n], tat[n];
+
+    findWaitingTime(processes, n, bt, wt);
+    findTurnAroundTime(processes, n, bt, wt, tat);
+
+    printf("Process\tBurst Time\tWaiting Time\tTurnaround Time\n");
+    float total_wt = 0, total_tat = 0;
+    for (int i = 0; i < n; i++) {
+        total_wt += wt[i];
+        total_tat += tat[i];
+        printf("%d\t%d\t\t%d\t\t%d\n", processes[i], bt[i], wt[i], tat[i]);
+    }
+    
+    printf("Average Waiting Time: %.2f\n", total_wt / n);
+    printf("Average Turnaround Time: %.2f\n", total_tat / n);
+}
+
+void sortProcesses(int processes[], int n, int bt[]) {
+    // Simple selection sort based on burst time
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (bt[j] < bt[i]) {
+                // Swap burst time
+                int temp = bt[i];
+                bt[i] = bt[j];
+                bt[j] = temp;
+
+                // Swap process ID
+                temp = processes[i];
+                processes[i] = processes[j];
+                processes[j] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int processes[] = {1, 2, 3};
+    int burst_time[] = {6, 8, 7};
+    int n = sizeof(processes) / sizeof(processes[0]);
+
+    // Sort processes based on burst time
+    sortProcesses(processes, n, burst_time);
+
+    findavgTime(processes, n, burst_time);
+    return 0;
+}
