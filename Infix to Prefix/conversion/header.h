66a6de5ac2e4c2a1e654247168dac281a55cfac9
Yash Kumar Saini <115717039+yashksaini-coder@users.noreply.github.com>
2024-10-16 18:29:52 +0000

    Add header files

---
+++ Infix to Prefix/conversion/header.h
@@ -0,0 +1,10 @@
+#ifndef INFIX_TO_PREFIX_H
+#define INFIX_TO_PREFIX_H
+
+#define MAX 100
+
+// Function declarations
+void infixToPrefix(const char* infix, char* prefix);
+int isValidInfix(const char* exp);
+
+#endif
