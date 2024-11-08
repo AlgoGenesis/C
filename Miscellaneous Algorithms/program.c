3a17f94244245ba5dd1126ce19560bf1fa28638d
Rajnil Saha <rajnilhere@gmail.com>
2024-11-08 21:56:03 +0530

    Create program.c

---
+++ Miscellaneous Algorithms/program.c
@@ -0,0 +1,13 @@
+
+int climbStairs(int n) {
+        if(n<1){
+            return 0;
+        }
+        int dp[100];
+        dp[0] =1;
+        dp[1] = 2;
+        for(int i= 2; i<n ;i++) {
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n-1];
+    }
